0c573d3eb2908f5aa5d730b58a4130888f62c283
Madison Oliver <moliver20@my.whitworth.edu>
2018-05-12 14:09:19 -0700

    Finished polymorphism in the Caregivers file with the Caregivers, Doctors, and Nurses classes

--- Hospital Simulator/Hospital Simulator/Caregivers.hpp
+++ Hospital Simulator/Hospital Simulator/Caregivers.hpp
@@ -10,18 +10,29 @@
 #define Caregivers_hpp
 
 #include <stdio.h>
+#include "Random.h"
 
 //parent class for Doctors and Nurses
 class Caregivers
 {
-    
 public:
+    Caregivers() {}
     int getTreatTime();
 };
 
-class Nurses
+//Nurses class, use polymorphism to get the correct time to treat a patient for Nurses
+class Nurses : public Caregivers
 {
-    
+public:
+    Nurses():Caregivers() {}
+    int getTreatTime();
+};
+
+//Doctors class, uses Caregivers' function to get correct time to treat a patient for Doctors
+class Doctors : public Caregivers
+{
+public:
+    Doctors() : Caregivers() {}
 };
 
 #endif /* Caregivers_hpp */
--- Hospital Simulator/Hospital Simulator/Random.h
+++ Hospital Simulator/Hospital Simulator/Random.h
@@ -9,5 +9,39 @@
 #ifndef Random_h
 #define Random_h
 
+#include <cstdlib>
+#include <ctime>
+
+/** Class to encapsulate the standard random number generator. */
+class Random {
+    
+public:
+    
+    /** Initializes the random number generator using the time
+     as the seed.
+     */
+    Random() {
+        srand((unsigned int)std::time(0));
+    }
+    
+    /** Initializes the randon mumber generator using a
+     supplied seed.
+     */
+    Random(int seed) {
+        srand(seed);
+    }
+    
+    /** Returns a random integer in the range 0 ñ n. */
+    int next_int(int n) {
+        return int(next_double() * n);
+    }
+    
+    /** Return a random double in the range 0 ñ 1. */
+    double next_double() {
+        return double(rand()) / RAND_MAX;
+    }
+    
+};
+
 
 #endif /* Random_h */
