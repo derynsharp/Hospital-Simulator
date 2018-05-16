a4dc89cbef0a1d5d5421ccbb380566a664966932
Madison Oliver <moliver20@my.whitworth.edu>
2018-05-16 15:21:12 -0700

    Some minor changes to Random

--- Hospital Simulator/Hospital Simulator/Random.h
+++ Hospital Simulator/Hospital Simulator/Random.h
@@ -8,32 +8,26 @@
 #include <cstdlib>
 #include <ctime>
 
-/** Class to encapsulate the standard random number generator. */
+//used to randomize the amount of time it takes for Doctors and Nurses to treat a patient
 class Random {
     
 public:
-    
-    /** Initializes the random number generator using the time
-     as the seed.
-     */
-    Random() {
+    Random()
+    {
         srand((unsigned int)std::time(0));
     }
-    
-    /** Initializes the randon mumber generator using a
-     supplied seed.
-     */
-    Random(int seed) {
+    Random(int seed)
+    {
         srand(seed);
     }
-    
-    /** Returns a random integer in the range 0 ñ n. */
-    int next_int(int n) {
+  
+    int next_int(int n)
+    {
         return int(next_double() * n);
     }
     
-    /** Return a random double in the range 0 ñ 1. */
-    double next_double() {
+    double next_double()
+    {
         return double(rand()) / RAND_MAX;
     }
     
